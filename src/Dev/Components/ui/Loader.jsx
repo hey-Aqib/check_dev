60ed0c342d0f8a08c66d03cf213b6568f9eeea7b
hey-Aqib <Phantomdevel@gmail.com>
2025-05-03 21:53:43 +0500

    yayy

--- src/Dev/Components/ui/Loader.jsx
+++ src/Dev/Components/ui/Loader.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Loader = () => {
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+    <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
       <div className="w-16 h-16 border-4 border-t-transparent border-white rounded-full animate-spin"></div>
     </div>
   );
